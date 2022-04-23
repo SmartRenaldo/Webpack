43509afda2a7c5eb07b9bcf9e0b6755dc52271c3
SmartRenaldo <102466979+SmartRenaldo@users.noreply.github.com>
2022-04-23 23:48:21 +0930

    Add files via upload

---
+++ part1/06-manage-assets/webpack.config.js
@@ -0,0 +1,124 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const toml = require('toml')
+const yaml = require('yaml')
+const json5 = require('json5')
+
+module.exports = {
+    entry: './src/index.js',
+
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, './dist'),
+        clean: true,
+        assetModuleFilename: 'images/[contenthash][ext]'
+    },
+
+    mode: 'development',
+
+    devtool: 'inline-source-map',
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html',
+            filename: 'app.html',
+            inject: 'body'
+        }),
+
+        new MiniCssExtractPlugin({
+            filename: 'styles/[contenthash].css'
+        })
+    ],
+
+    devServer: {
+        static: './dist'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.png$/,
+                type: 'asset/resource',
+                // the proproty of generator is higher than assetModuleFilename
+                generator: {
+                    filename: 'images/[contenthash][ext]'
+                }
+            },
+
+            {
+                test: /\.svg$/,
+                type: 'asset/inline'
+            },
+
+            {
+                test: /\.txt$/,
+                type: 'asset/source'
+            },
+
+            // ALT + Z 折行 （Toggle word wrap）
+            // By default, in 'asset' type, webpack will automatically choose between resource and inline by default: files smaller than 8kb will be treated as inline modules, otherwise they will be treated as resource modules
+            {
+                test: /\.jpg$/,
+                type: 'asset',
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: 4 * 1024 * 1024,
+                    }
+                }
+            },
+
+            {
+                test: /\.(css|less)$/,
+                // load from back to front
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'less-loader']
+            },
+
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                type: 'asset/resource'
+            },
+
+            {
+                test: /\.(csv|tsv)$/,
+                use: 'csv-loader'
+            },
+
+            {
+                test: /\.xml$/,
+                use: 'xml-loader'
+            },
+
+            {
+                test: /\.toml$/,
+                type: 'json',
+                parser: {
+                    parse: toml.parse
+                }
+            },
+
+            {
+                test: /\.yaml$/,
+                type: 'json',
+                parser: {
+                    parse: yaml.parse
+                }
+            },
+
+            {
+                test: /\.json5$/,
+                type: 'json',
+                parser: {
+                    parse: json5.parse
+                }
+            }
+        ]
+    },
+
+    optimization: {
+        minimizer: [
+            new CssMinimizerPlugin()
+        ]
+    }
+}
