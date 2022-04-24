4d72e54fcf638eea291356f0f432196380a83a7e
SmartRenaldo <102466979+SmartRenaldo@users.noreply.github.com>
2022-04-24 20:17:31 +0930

    Add files via upload

---
+++ part1/09-caching/dist/scripts/math.61cea2fd60927797aba6.js
@@ -0,0 +1,25 @@
+"use strict";
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["math"],{
+
+/***/ "./src/math.js":
+/*!*********************!*\
+  !*** ./src/math.js ***!
+  \*********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "add": () => (/* binding */ add),
+/* harmony export */   "minus": () => (/* binding */ minus)
+/* harmony export */ });
+var add = function add(x, y) {
+  return x + y;
+};
+var minus = function minus(x, y) {
+  return x - y;
+};
+
+/***/ })
+
+}]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic2NyaXB0cy9tYXRoLjYxY2VhMmZkNjA5Mjc3OTdhYmE2LmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7O0FBQU8sSUFBTUEsR0FBRyxHQUFHLFNBQU5BLEdBQU0sQ0FBQ0MsQ0FBRCxFQUFJQyxDQUFKLEVBQVU7QUFDekIsU0FBT0QsQ0FBQyxHQUFHQyxDQUFYO0FBQ0gsQ0FGTTtBQUlBLElBQU1DLEtBQUssR0FBRyxTQUFSQSxLQUFRLENBQUNGLENBQUQsRUFBSUMsQ0FBSixFQUFVO0FBQzNCLFNBQU9ELENBQUMsR0FBR0MsQ0FBWDtBQUNILENBRk0iLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9zcmMvbWF0aC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgY29uc3QgYWRkID0gKHgsIHkpID0+IHtcclxuICAgIHJldHVybiB4ICsgeTtcclxufVxyXG5cclxuZXhwb3J0IGNvbnN0IG1pbnVzID0gKHgsIHkpID0+IHtcclxuICAgIHJldHVybiB4IC0geTtcclxufVxyXG4iXSwibmFtZXMiOlsiYWRkIiwieCIsInkiLCJtaW51cyJdLCJzb3VyY2VSb290IjoiIn0=
