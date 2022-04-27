640f21e58e63bf04dad38d37a289adb9978b6957
SmartRenaldo <102466979+SmartRenaldo@users.noreply.github.com>
2022-04-27 10:14:39 +0930

    Add files via upload

---
+++ part2/07-shimming/01-third-party-shimming/src/globals.js
@@ -0,0 +1,10 @@
+const file = 'example.txt'
+const helpers = {
+  test: function(){
+    console.log('testing.......');
+  },
+
+  parse: function () {
+    console.log('parsing......');
+  }
+}
---
+++ part2/08-library/03-test/app.js
@@ -0,0 +1,2 @@
+const webpackNumbers = require('02-webpack-numbers-testingtest')
+console.log(webpackNumbers.wordToNum('One'))
